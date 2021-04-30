152316b2544c56f256f0159ff2578e702ca33e0a
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2021-05-01 01:55:01 +0500

    4 styled components

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
 import SecvicesSection from '../components/SecvicesSection'
+import Faq from '../components/FaqSection'
 const AboutUs = () => {
     return (
         <div>
             <AboutSection />
             <SecvicesSection />
+            <Faq />
         </div>
     )
 }
